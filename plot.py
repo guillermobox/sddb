d6121b21277240d0644fed09e97543b5fc7a979b
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2018-06-15 14:10:48 +0200

    Make better plots

--- plot.py
+++ plot.py
@@ -1,5 +1,15 @@
 import h5py
+import matplotlib
+
+matplotlib.use('Qt5Agg')
+
 import matplotlib.pyplot as plt
+
+
+plt.style.use('/home/indalecio/Desktop/TU/guille.mplstyle')
+
+matplotlib.rcParams['figure.figsize'] = (3.5, 2.7)
+matplotlib.rcParams['savefig.dpi'] = 300
 
 def label_from_dataset(dset):
     label = '{0} ({1})'.format(dset.attrs['fullname'][0].decode('utf8'), dset.attrs['units'][0].decode('utf8'))
@@ -14,15 +24,36 @@ groupname = li.path
 current = group['current']
 voltage = group['voltage']
 
-title = "Plotting {0}/{1}\n".format(filename,groupname)
+filename = "{0}/{1}".format(filename,groupname)
+plt.annotate(filename, (0.02, 0.98),
+    xycoords='figure fraction',
+    verticalalignment='top',
+    horizontalalignment='left',
+    family='monospace',
+)
+
+if 'creation_time' in group.attrs:
+    time = group.attrs['creation_time'][0].decode('ascii')
+    plt.annotate(time, (0.98, 0.98),
+        xycoords='figure fraction',
+        verticalalignment='top',
+        horizontalalignment='right',
+        family='monospace',
+    )
+
+title = ""
 
 for at in group.attrs:
-    title = title + at + " = " + str(group.attrs[at]) + '\n'
+    if at == 'creation_time':
+        continue
+    title = title + at + " = " + str(group.attrs[at]) + " "
 
 plt.plot(voltage.value, current.value, 'k-')
 plt.title(title.strip())
 
 plt.xlabel(label_from_dataset(voltage))
 plt.ylabel(label_from_dataset(current))
+plt.gcf().tight_layout()
+plt.subplots_adjust(top=0.8)
 
 plt.show()
