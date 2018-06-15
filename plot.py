67c1eeac1fa1ae2bcefe2ea0cde4957162191e2e
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2018-06-15 17:31:28 +0200

    Annotate simulation info in box

--- plot.py
+++ plot.py
@@ -6,7 +6,6 @@ matplotlib.use('Qt5Agg')
 
 import matplotlib.pyplot as plt
 
-
 plt.style.use('/home/indalecio/Desktop/TU/guille.mplstyle')
 
 matplotlib.rcParams['figure.figsize'] = (3.5, 2.7)
@@ -52,23 +51,28 @@ if 'creation_time' in group.attrs:
         family='monospace',
     )
 
-title = ""
+title = "$\\it{0}$\n".format(filename)
 
 for at in group.attrs:
     if at == 'creation_time':
         continue
-    title = title + at + " = " + str(group.attrs[at]) + " "
+    title = title + at + " = " + str(group.attrs[at]) + "\n"
+
+plt.gca().annotate(title.strip(),
+            xy=(0.05, 0.95), xycoords='axes fraction',
+            horizontalalignment='left', verticalalignment='top',
+            bbox=dict(boxstyle="square,pad=.5", fc=(.9, .9, .9), lw=1),
+)
 
 plt.plot(voltage.value, current.value,
     color='black',
     linestyle='solid',
     linewidth=1
 )
-plt.title(title.strip())
 
 plt.xlabel(label_from_dataset(voltage))
 plt.ylabel(label_from_dataset(current))
 plt.gcf().tight_layout()
-plt.subplots_adjust(top=0.8)
+plt.subplots_adjust(top=0.9)
 
 plt.show()
